81737d3f8eecd5182ba12ceb29ed9672ef08794b
Kristina <the.frog.1raveler@gmail.com>
2025-04-23 23:01:01 +0600

    Функция отчистки формы и установка disabled для кнопки формы

--- frontend/src/js/helpers/reset-form.js
+++ frontend/src/js/helpers/reset-form.js
@@ -7,31 +7,10 @@ import {
     SUBMIT_BUTTON,
 } from "../constants/global.js";
 
-
-// Функция отчистки формы и установка disabled для кнопки формы
-export const resetForm = () => {
-    FORM.reset();
-    SUBMIT_BUTTON.disabled = true;
-    FORM.addEventListener("input", () => {
-        if (
-            FIRST_NAME_INPUT.value.trim() !== "" &&
-            SURNAME_INPUT.value.trim() !== "" &&
-            ADDRESS_INPUT.value.trim() !== "" &&
-            AGE_INPUT.value.trim() !== ""
-        ) {
-            SUBMIT_BUTTON.disabled = false;
-        } else {
-            SUBMIT_BUTTON.disabled = true;
-        }
-    });
-};
-
-// Добавляем обработчик события на отправку формы
-
+// Функция отчистки формы и установка disabled для кнопки формы.
 FORM.addEventListener("submit", async (e) => {
     e.preventDefault();
 
-    // Собираем данные
     const payload = {
         firstName: FIRST_NAME_INPUT.value.trim(),
         surname: SURNAME_INPUT.value.trim(),
@@ -41,19 +20,15 @@ FORM.addEventListener("submit", async (e) => {
 
     try {
         if (STATE.editingStudentId) {
-            // PUT — обновляем
             await updateStudentData(STATE.editingStudentId, payload);
         } else {
-            // POST — добавляем
             await addStudentAPI(payload);
         }
 
-        // Сброс формы и состояния
         FORM.reset();
         STATE.editingStudentId = null;
         FORM.querySelector("button").textContent = "Добавить студента";
 
-        // Обновляем таблицу
         await loadJSON();
     } catch (err) {
         console.error("Ошибка при сохранении:", err);
